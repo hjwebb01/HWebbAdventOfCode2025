f49817348f82983ee2b5b06b09846c402e3968ef
Hunter W <hjwebb9@gmail.com>
2025-12-02 19:32:43 -0500

    part 1 answer

---
+++ day2/day2.js
@@ -0,0 +1,37 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs_1 = require("fs");
+function adventDay2(range) {
+    var bounds = range.split("-").map(Number); // Split the range into lower and upper bounds
+    var badID = 0;
+    for (var i = bounds[0]; i <= bounds[1]; i++) {
+        // Loop through each number in the range
+        var numStr = i.toString();
+        if (numStr.length % 2 != 0)
+            continue; // Skip if the number of digits is odd
+        var l = 0;
+        var mid = numStr.length / 2;
+        var valid = true;
+        while (mid < numStr.length) {
+            if (numStr[l] != numStr[mid]) {
+                valid = false;
+            }
+            l = l + 1;
+            mid = mid + 1;
+        }
+        if (valid) {
+            badID = badID + i;
+        }
+    }
+    return badID;
+}
+function main() {
+    var input = (0, fs_1.readFileSync)("./input.txt", "utf8");
+    var res = 0;
+    input.split(",").forEach(function (line) {
+        // Split by comma
+        res = res + adventDay2(line);
+    });
+    console.log(res); // print result
+}
+main();
--- day2/day2.ts
+++ day2/day2.ts
@@ -1,16 +1,38 @@
-import fs from 'fs';
+import { readFileSync } from "fs";
 
-function adventDay2(range){
-  return 0;
+function adventDay2(range) {
+  const bounds = range.split("-").map(Number); // Split the range into lower and upper bounds
+  var badID = 0;
+  for (let i = bounds[0]; i <= bounds[1]; i++) {
+    // Loop through each number in the range
+    const numStr = i.toString();
+    if (numStr.length % 2 != 0) continue; // Skip if the number of digits is odd
+
+    var l = 0;
+    var mid = numStr.length / 2;
+    var valid = true;
+    while (mid < numStr.length) {
+      if (numStr[l] != numStr[mid]) {
+        valid = false;
+      }
+      l = l + 1;
+      mid = mid + 1;
+    }
+    if (valid) {
+      badID = badID + i;
+    }
+  }
+  return badID;
 }
-
-
 
 function main() {
-  const input = fs.readFileSync('day2/input.txt', 'utf8');
+  const input = readFileSync("./input.txt", "utf8");
   var res = 0;
-  input.split(',').forEach(line => { // Split by comma
-     res = res + adventDay2(line);
-  })
+  input.split(",").forEach((line) => {
+    // Split by comma
+    res = res + adventDay2(line);
+  });
   console.log(res); // print result
 }
+
+main();
